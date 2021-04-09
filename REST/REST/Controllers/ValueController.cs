27562707ce006213a94139efb45e62ec741ede39
RostyslavBoretskiy <33392714+RostyslavBoretskiy@users.noreply.github.com>
2021-04-09 05:35:56 +0300

    Add files via upload

---
+++ REST/REST/Controllers/ValueController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REST.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping()
+            => "localhost";
+
+        [HttpGet]
+        public string Echo(string text)
+            => $"Its echo from server with text: { text }";
+
+        [HttpPost]
+        public bool Login(string name, string pass)
+            => name == "rostyk" && pass == "111";
+    }
+}
