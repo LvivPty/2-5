641c076c858b7cf588f2a56bd383e9d9ec01b1b6
RostyslavBoretskiy <33392714+RostyslavBoretskiy@users.noreply.github.com>
2021-04-09 05:37:53 +0300

    Add files via upload

---
+++ MQ/MQ/Core/MessageQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQ.Core
+{
+    public static class MessageQueue
+    {
+        public static List<string> Messages = new List<string>() { };
+
+        public static void Create(string message)
+        {
+            Messages.Add(message);
+        }
+
+        internal static bool Exists(string v)
+        {
+            return Messages.Contains(v);
+        }
+
+        public static bool Delete(string message)
+        {
+            return Messages.Remove(message);
+        }
+        public static List<string> GetMessages()
+        {
+            return Messages;
+        }
+    }
+}
---
+++ MQ/MQ/Program.cs
@@ -0,0 +1,65 @@
+﻿using MQ.Core;
+using System;
+
+namespace MQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Create message queue");
+                Console.WriteLine("2. Delete message queue");
+                Console.WriteLine("3. Get message queue");
+
+                var command = int.Parse(Console.ReadLine());
+
+                if (command == 1)
+                {
+                    Console.WriteLine("Enter QM name: ");
+                    CreateMQ(Console.ReadLine());
+                }
+                else if(command == 2)
+                {
+                    Console.WriteLine("Enter QM name: ");
+                    DeleteMQ(Console.ReadLine());
+                }
+                else if(command == 3)
+                {
+                    PrintMQ();
+                }
+            }
+        }
+
+        static void CreateMQ(string name)
+        {
+            if (MessageQueue.Exists($@".\\{name}"))
+            {
+                Console.WriteLine($"Queue with name: { name } already exist.");
+            }
+            else
+            {
+                Console.WriteLine($"Queue .\\{name} not Found");
+            }
+            MessageQueue.Create($@".\{name}");
+            Console.WriteLine("Queue Created.");
+        }
+        static void DeleteMQ(string name)
+        {
+            var res = MessageQueue.Delete(name);
+            if(res)
+                Console.WriteLine($".\\{name} deleted.");
+            else
+                Console.WriteLine($".\\{name} not deleted.");
+        }
+
+        static void PrintMQ()
+        {
+            foreach (var queue in MessageQueue.GetMessages())
+            {
+                Console.WriteLine(queue);
+            }
+        }
+    }
+}
